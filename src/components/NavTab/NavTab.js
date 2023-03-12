62e6959f7e7f06b59604fd21dcc5a0fdf27411c9
Alex Gus <radiogusarov@gmail.com>
2023-03-12 08:01:57 +0300

    add Promo

---
+++ src/components/NavTab/NavTab.js
@@ -0,0 +1,13 @@
+function NavTab() {
+  return (
+    <nav className="navtab">
+      <ul className="navtab__list">
+        <li className="navtab__link">О проекте</li>
+        <li className="navtab__link">Технологии</li>
+        <li className="navtab__link">Студент</li>
+      </ul>
+    </nav>
+  )
+};
+
+export default NavTab;
