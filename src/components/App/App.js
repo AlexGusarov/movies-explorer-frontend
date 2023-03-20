30712401aa3b9c26801b86d82ee3b944970c33f3
Alex Gus <radiogusarov@gmail.com>
2023-03-20 16:41:22 +0300

    Add NoMatch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Main from '../Main/Main';
+import NoMatch from '../NoMatch/NoMatch';
 
 function App() {
   return (
     <div className='root'>
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
-
   )
-
 };
 
 export default App;
