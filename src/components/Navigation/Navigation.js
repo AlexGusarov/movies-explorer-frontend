b5445dcfcb9d5853f9bdde21e1dd66b2cb960a73
Alex Gus <radiogusarov@gmail.com>
2023-03-29 00:00:15 +0300

    Add useMediaQuery to Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from "react-responsive";
 import ProfileButton from '../ProfileButton/ProfileButton';
 import Burger from '../Burger/Burger';
 
+
 function Navigation({ loggedIn }) {
+
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+
   return (
     <div className="navigation">
       {(!loggedIn)
@@ -14,16 +20,19 @@ function Navigation({ loggedIn }) {
         :
         <>
           <div className='navigation__links-container'>
-            <div className='navigation__link-wrapper'>
-              <Link to="/movies" className="navigation__link navigation__link_logged-out">Фильмы</Link>
-              <Link to="/saved-films" className="navigation__link navigation__link_logged-out">Сохраненные фильмы</Link>
-            </div>
-            {<Burger />}
-            {/* <Link to="/sign-in" className="navigation__link">
-              {<ProfileButton />}             
-            </Link> */}
+            {isDesktopOrLaptop && (
+              <>
+                <div className='navigation__link-wrapper'>
+                  <Link to="/movies" className="navigation__link navigation__link_logged-out">Фильмы</Link>
+                  <Link to="/saved-films" className="navigation__link navigation__link_logged-out">Сохраненные фильмы</Link>
+                </div>
+                <Link to="/sign-in" className="navigation__link navigation__link-profile">
+                  {<ProfileButton />}
+                </Link>
+              </>
+            )}
+            {isTabletOrMobile && <Burger />}
           </div>
-
         </>
       }
     </div>
