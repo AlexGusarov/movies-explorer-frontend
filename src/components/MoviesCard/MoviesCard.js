fd62521a76a1a34e32e4b42fdf276afb17a6a69d
Alex Gus <radiogusarov@gmail.com>
2023-04-29 23:08:11 +0300

    Change favicon

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './MoviesCard.css';
 import minToHours from '../../utils/minToHours';
 
 function MoviesCard(props) {
+  const [saved, setSaved] = useState(false);
+
+  function handleClick() {
+    setSaved(!saved);
+    // + здесь будет вызвана функция сохранения фильмов//
+  }
+
   return (
     <li className="movies-card">
       <div className="movies-card__container">
@@ -9,7 +17,7 @@ function MoviesCard(props) {
           <p className="movies-card__name">{props.nameRU}</p>
           <span className="movies-card__duration">{minToHours(props.duration)}</span>
         </div>
-        <button className="movies-card__button" aria-label="Save" />
+        <button className={`movies-card__button ${saved ? 'movies-card__button_saved' : ''}`} aria-label="Save" onClick={handleClick} />
       </div>
       <a href={props.trailerLink} className="movies-card__link">
         <img className="movies-card__image" src={props.image} alt="Cмотреть трейлер фильма" />
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,17 +1,53 @@
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import Search from "../Search/Search";
+import Switch from "../Switch/Switch";
 
 function MoviesCardList({ cards }) {
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+
+  const [value, setValue] = useState(false);
+  const [cardQuantity, setCardQuantity] = useState(12);
+  const [shownCards, setShownCards] = useState(cards);
+
+  function handleClickMore() {
+    setCardQuantity(cardQuantity + 3);
+    setTimeout(() => {
+      window.scrollBy({
+        top: 320,
+        behavior: 'smooth',
+      });
+    }, 100)
+  }
+
+  useEffect(() => {
+    if (isDesktopOrLaptop) {
+      setCardQuantity(12)
+    }
+
+    if (isTabletOrMobile) {
+      setCardQuantity(8)
+    }
+    setShownCards(cards.slice(0, cardQuantity))
+  }, [cards, cardQuantity, isDesktopOrLaptop, isTabletOrMobile]);
+
   return (
     <>
       <Search />
+      <div className="switch-box">
+        <Switch isOn={value} handleToggle={() => setValue(!value)} />
+        <span className="switch-box__label">Короткометражки</span>
+      </div>
       <section className="movies-card-list">
         <ul className="movies-card-list__list">
-          {cards.map((item, index) => (
+          {shownCards.map((item, index) => (
             //для этапа вёрстки, потом для key будет movieId
             <MoviesCard props={item} {...item} key={`${index}`} />
           ))}
         </ul>
+        <button className="movies-card-list__button-more" onClick={handleClickMore}>Ещё</button>
       </section>
     </>
   )
